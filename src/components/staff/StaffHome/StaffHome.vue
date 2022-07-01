6e05ea5abd032581798b91135ab762f85150ddda
cyyyy-y <327234415@qq.com>
2022-07-01 21:28:09 +0800

    员工主页完善

--- src/components/staff/StaffHome/StaffHome.vue
+++ src/components/staff/StaffHome/StaffHome.vue
@@ -50,15 +50,83 @@
 
 <!--    快捷入口-->
     <el-row class="QEntry">
-      <el-col :span="24">
-        <div>1</div>
+      <el-col :span="24" class="QE1">
+        <div class="QE-line"></div>
+        <div class="QE-text">快捷入口</div>
+      </el-col>
+      <el-col :span="24" class="QE2">
+        <el-row :gutter="20">
+          <el-col :span="2">
+            <div class="QE2-mini">
+              <div class="QE2-mini-icon"><img src="../../../assets/QE1.png"/></div>
+              <div class="QE2-mini-text">违约查询</div>
+            </div>
+          </el-col>
+
+          <el-col :span="2">
+            <div class="QE2-mini">
+              <div class="QE2-mini-icon"><img src="../../../assets/QE2.png"/></div>
+              <div class="QE2-mini-text">违约申请</div>
+            </div>
+          </el-col>
+
+          <el-col :span="2">
+            <div class="QE2-mini">
+              <div class="QE2-mini-icon"><img src="../../../assets/QE3.png"/></div>
+              <div class="QE2-mini-text">违约重生</div>
+            </div>
+          </el-col>
+
+          <el-col :span="2">
+            <div class="QE2-mini">
+              <div class="QE2-mini-icon"><img src="../../../assets/QE4.png"/></div>
+              <div class="QE2-mini-text">自定义</div>
+            </div>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
 
 <!--    消息提醒 & 申请数量图表-->
-    <el-row class="MessChart">
-      <el-col :span="24">
-        <div>1</div>
+    <el-row :gutter="20" class="MessChart">
+<!--      消息提醒-->
+      <el-col :span="16">
+        <div class="mess">
+          <div class="mess1">
+            <div class="QE-line"></div>
+            <div class="QE-text">消息提醒</div>
+          </div>
+
+          <div class="mess2">
+            <template>
+              <el-table
+                      :data="tableData"
+                      stripe
+                      :show-header="false"
+                      style="width: 97%;height: 220px;margin-top: 20px;margin-left: 12px">
+                <el-table-column
+                        prop="pass"
+                        label="是否通过"
+                        width="150">
+                </el-table-column>
+                <el-table-column
+                        prop="message"
+                        label="详细信息"
+                        width="380">
+                </el-table-column>
+                <el-table-column
+                        prop="time"
+                        label="时间">
+                </el-table-column>
+              </el-table>
+            </template>
+          </div>
+        </div>
+      </el-col>
+
+<!--      申请数量图表-->
+      <el-col :span="8">
+        <div class="chart">1</div>
       </el-col>
     </el-row>
 
@@ -67,7 +135,28 @@
 
 <script>
 export default {
-  name: "StaffHome"
+  name: "StaffHome",
+  data() {
+    return {
+      tableData: [{
+        pass: '已通过',
+        message: 'XXX申请与XX年X月X日被通过，审核人XXX ...',
+        time: '11-02 09:09'
+      }, {
+        pass: '已驳回',
+        message: 'XXX申请与XX年X月X日被驳回，审核人XXX ...',
+        time: '11-02 09:09'
+      }, {
+        pass: '新增',
+        message: 'XX年X月X日新增XXX申请 ...',
+        time: '11-02 09:09'
+      }, {
+        pass: '审核中',
+        message: 'XXX申请正在审核中，审核人XXX ...',
+        time: '11-02 09:09'
+      }]
+    }
+  }
 }
 </script>
 
@@ -89,13 +178,18 @@ export default {
   }
 
   .QEntry {
-    background-color: lightblue;
+    background-color: #ffffff;
     height: 25%;
+    border-radius: 4px;
+    margin-left: 0px!important;
+    margin-right: 0px!important;
   }
 
   .MessChart {
     background-color: beige;
-    height: 50%;
+    height: 44%;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
   }
 
   /*card*/
@@ -109,7 +203,7 @@ export default {
 
   .card-mini {
     display: flex;
-    justify-content: center;
+    /*justify-content: center;*/
     align-items: center;
   }
 
@@ -160,6 +254,7 @@ export default {
     justify-content: center;
     align-items: center;
     background-color: #f4dbd7;
+    margin-left: 35px;
   }
 
   .card-m21 {
@@ -202,6 +297,7 @@ export default {
     justify-content: center;
     align-items: center;
     background-color: #dbd6ee;
+    margin-left: 65px;
   }
 
   .card-m31 {
@@ -239,5 +335,71 @@ export default {
     font-weight: bold;
   }
 
+  .card-m1, .card-m2, .card-m3 {
+    border-radius: 4px;
+  }
+
+
+  /*QEntry*/
+  .QE1 {
+    display: flex;
+    /*background-color: #ffffff;*/
+  }
+
+  .QE-line {
+    height: 21px;
+    width: 3px;
+    background-color: #11327F;
+    margin-left: 15px;
+    margin-top: 13px;
+    margin-right: 10px;
+  }
+
+  .QE-text {
+    margin-top: 13px;
+    font-weight: bold;
+    font-size: 14px;
+  }
+
+  .QE2 {
+    padding-left: 25px;
+  }
+
+  .QE2-mini {
+    /*background-color: #7e7fa5;*/
+  }
+
+  .QE2-mini-icon {
+    margin-left: 5px;
+    margin-top: 25px;
+    margin-bottom: 10px;
+  }
+
+  .QE2-mini-text {
+    font-size: 14px;
+  }
+
+
+/*MessChart*/
+  .mess {
+    background-color: #ffffff;
+  }
+
+  .mess1 {
+    display: flex;
+  }
+
+  .mess-line {
+    height: 21px;
+    width: 3px;
+    background-color: #11327F;
+    margin-left: 15px;
+    margin-top: 13px;
+    margin-right: 10px;
+  }
+
+  .chart {
+    background-color: #FCAB61;
+  }
 
 </style>
